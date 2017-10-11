2a786b1ae78372b23498b9d250df264943467951
MiguelArber <miguel.arber@gmail.com>
2017-10-11 17:10:42 +0200

    Fixed a testing bug

--- src/test/java/APAW/ECP2/MiguelArber/api/controllers/ArticleControllerIT.java
+++ src/test/java/APAW/ECP2/MiguelArber/api/controllers/ArticleControllerIT.java
@@ -21,7 +21,7 @@ public class ArticleControllerIT {
     @Test
     public void testCreateAndArticleList() {
     	articleController.createArticle("article");
-        assertEquals("aricle", articleController.articleList().get(0).getReference());
+        assertEquals("article", articleController.articleList().get(0).getReference());
     }
     
 }
