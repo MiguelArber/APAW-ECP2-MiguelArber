344a1f7d8f1e833b5e0ceba70c64ba171b80cb66
MiguelArber <miguel.arber@gmail.com>
2017-10-11 12:53:35 +0200

    Added Controller, entity builders and tests

--- src/main/java/APAW/ECP2/MiguelArber/api/controllers/ShoppingCartController.java
+++ src/main/java/APAW/ECP2/MiguelArber/api/controllers/ShoppingCartController.java
@@ -4,12 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import APAW.ECP2.MiguelArber.api.entities.builders.ShoppingCartBuilder;
 import APAW.ECP2.MiguelArber.api.daos.DaoFactory;
 import APAW.ECP2.MiguelArber.api.dtos.ShoppingCartDto;
 import APAW.ECP2.MiguelArber.api.entities.ShoppingCart;
 import APAW.ECP2.MiguelArber.api.dtos.ShoppingCartArticleListDto;
 
 public class ShoppingCartController {
+	
+	private int next_ID = 0;
+	
+    public void createShoppingCart(String shoppingCartName) {
+        DaoFactory.getFactory().getShoppingCartDao().create(new ShoppingCartBuilder().addId(next_ID+1).addName(shoppingCartName).build());
+        next_ID++;
+    }
 
 
 }
